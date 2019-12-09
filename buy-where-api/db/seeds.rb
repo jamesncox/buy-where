da471c0eb4cf19ed95d6eaefbb33476130d130a2
jamesncox <jamesncox@yahoo.com>
2019-12-09 11:23:25 -0600

    Create seed data

--- buy-where-api/db/seeds.rb
+++ buy-where-api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Store.create!([{ name: 'Kroger' }, { name: 'Target' }, { name: 'Lucky' }])
+
+Item.create(name: 'Milk', price: 3.99, quantity: 1, store_id: 1)
+Item.create(name: 'Quilt', price: 70.00, quantity: 2, store_id: 2)
+Item.create(name: 'Bomber Jacket', price: 150.69, quantity: 1, store_id: 3,)
+Item.create(name: 'Skinny Jeans', price: 85.99, quantity: 1, store_id: 3,)
+Item.create(name: 'Top Gun T-shirt', price: 35.99, quantity: 1, store_id: 3,)
