1cacf1e9656e9ce2004aa6d33a387636b75c0b6b
jamesncox <jamesncox@yahoo.com>
2019-12-09 16:07:58 -0600

    Add StoresAdapter class to StoresAdapter.js

---
+++ buy-where-frontend/src/adapters/StoresAdapter.js
@@ -0,0 +1,37 @@
+class NotesAdapter {
+    constructor() {
+        this.baseUrl = 'http://localhost:3000/api/v1/stores'
+    }
+
+    getStores() {
+        return fetch(this.baseUrl).then(res => res.json())
+    }
+
+    createStore(value) {
+        const store = {
+            body: value,
+        }
+
+        return fetch(this.baseUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ store }),
+        }).then(res => res.json())
+    }
+
+    updateStore(value, id) {
+        const store = {
+            body: value,
+        }
+
+        return fetch(`${this.baseUrl}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ store }),
+        }).then(res => res.json())
+    }
+}
