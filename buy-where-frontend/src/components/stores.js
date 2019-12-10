ffba42ac3b4a946aa1a0ca84e468ba901e903bbe
jamesncox <jamesncox@yahoo.com>
2019-12-09 19:28:30 -0600

    Add event listeners to stores.js

---
+++ buy-where-frontend/src/components/stores.js
@@ -0,0 +1,70 @@
+class Stores {
+    constructor() {
+        this.stores = []
+        this.adapter = new StoresAdapter()
+        this.storeBindingsAndEventListeners()
+        this.itemBindingsAndEventListeners()
+        this.fetchAndLoadNotes()
+    }
+
+    storeBindingsAndEventListeners() {
+        this.storesContainer = document.getElementById('stores-container')
+        this.body = document.querySelector('body')
+        this.newStoreName = document.getElementById('store-name')
+        this.storeForm = document.getElementById('new-store-form')
+        this.storeForm.addEventListener('submit', this.createStore.bind(this))
+        this.storesContainer.addEventListener('dblclick', this.handleStoreClick.bind(this))
+        this.body.addEventListener('blur', this.updatStore.bind(this), true)
+    }
+
+    itemBindingsAndEventListeners() {
+
+    }
+
+    createStore(e) {
+        e.preventDefault()
+        const value = this.newStoreName.value
+
+        this.adapter.createStore(value).then(store => {
+            this.stores.push(new Store(store))
+            this.newStoreName.value = ''
+            this.render()
+        })
+    }
+
+    handleNoteClick(e) {
+        this.toggleStore(e)
+    }
+
+    toggleStore(e) {
+        const li = e.target
+        li.contentEditable = true
+        li.focus()
+        li.classList.add('editable')
+    }
+
+    updateStore(e) {
+        const li = e.target
+        li.contentEditable = false
+        li.classList.remove('editable')
+        const newValue = li.innerHTML
+        const id = li.dataset.id
+        //console.log(id)
+        this.adapter.updateStore(newValue, id)
+    }
+
+    fetchAndLoadStores() {
+        this.adapter
+            .getStores
+            .then(stores => {
+                stores.sort((a, b) => a.id - b.id).forEach(store => this.store.push(new Store(store)))
+            })
+            .then(() => {
+                this.render()
+            })
+    }
+
+    render() {
+        this.storesContainer.innerHTML = this.stores.map(store => store.renderLi()).join('')
+    }
+}
