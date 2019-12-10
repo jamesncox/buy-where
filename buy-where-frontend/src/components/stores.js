d38f056dcfcbe1182fae135ff880379dffeb6f0f
jamesncox <jamesncox@yahoo.com>
2019-12-10 08:28:41 -0600

    Refactor stores.js

--- buy-where-frontend/src/components/stores.js
+++ buy-where-frontend/src/components/stores.js
@@ -3,62 +3,63 @@ class Stores {
         this.stores = []
         this.lists = []
         this.adapter = new StoresAdapter()
-        this.storeBindingsAndEventListeners()
-        this.itemBindingsAndEventListeners()
-        this.fetchAndLoadNotes()
+        // this.storeBindingsAndEventListeners()
+        // this.itemBindingsAndEventListeners()
+        this.fetchAndLoadStores()
     }
 
-    storeBindingsAndEventListeners() {
-        this.storesContainer = document.getElementById('stores-container')
-        this.name = document.querySelector('name')
-        this.newStoreName = document.getElementById('store-name')
-        this.storeForm = document.getElementById('new-store-form')
-        this.storeForm.addEventListener('submit', this.createStore.bind(this))
-        this.storesContainer.addEventListener('dblclick', this.handleStoreClick.bind(this))
-        this.name.addEventListener('blur', this.updatStore.bind(this), true)
-    }
+    // storeBindingsAndEventListeners() {
+    //     this.storesContainer = document.getElementById('stores-container')
+    //     this.name = document.querySelector('name')
+    //     this.newStoreName = document.getElementById('store-name')
+    //     this.storeForm = document.getElementById('new-store-form')
+    //     this.storeForm.addEventListener('submit', this.createStore.bind(this))
+    //     this.storesContainer.addEventListener('dblclick', this.handleStoreClick.bind(this))
+    //     this.name.addEventListener('blur', this.updatStore.bind(this), true)
+    // }
 
-    itemBindingsAndEventListeners() {
+    // itemBindingsAndEventListeners() {
 
-    }
+    // }
 
-    createStore(e) {
-        e.preventDefault()
-        const value = this.newStoreName.value
+    // createStore(e) {
+    //     e.preventDefault()
+    //     const value = this.newStoreName.value
 
-        this.adapter.createStore(value).then(store => {
-            this.stores.push(new Store(store))
-            this.newStoreName.value = ''
-            this.render()
-        })
-    }
+    //     this.adapter.createStore(value).then(store => {
+    //         this.stores.push(new Store(store))
+    //         this.newStoreName.value = ''
+    //         this.render()
+    //     })
+    // }
 
-    handleNoteClick(e) {
-        this.toggleStore(e)
-    }
+    // handleNoteClick(e) {
+    //     this.toggleStore(e)
+    // }
 
-    toggleStore(e) {
-        const li = e.target
-        li.contentEditable = true
-        li.focus()
-        li.classList.add('editable')
-    }
+    // toggleStore(e) {
+    //     const li = e.target
+    //     li.contentEditable = true
+    //     li.focus()
+    //     li.classList.add('editable')
+    // }
 
-    updateStore(e) {
-        const li = e.target
-        li.contentEditable = false
-        li.classList.remove('editable')
-        const newValue = li.innerHTML
-        const id = li.dataset.id
-        //console.log(id)
-        this.adapter.updateStore(newValue, id)
-    }
+    // updateStore(e) {
+    //     const li = e.target
+    //     li.contentEditable = false
+    //     li.classList.remove('editable')
+    //     const newValue = li.innerHTML
+    //     const id = li.dataset.id
+    //     //console.log(id)
+    //     this.adapter.updateStore(newValue, id)
+    // }
 
     fetchAndLoadStores() {
         this.adapter
-            .getStores
+            .getStores()
             .then(stores => {
-                stores.sort((a, b) => a.id - b.id).forEach(store => this.store.push(new Store(store)))
+                console.log(stores)
+                stores.forEach(store => this.store.push(new Store(store)))
             })
             .then(() => {
                 this.render()
@@ -66,6 +67,7 @@ class Stores {
     }
 
     render() {
-        this.storesContainer.innerHTML = this.stores.map(store => store.renderLi()).join('')
+        this.storesContainer.innerHTML = this.stores.data.included.map(store => store.renderLi()).join('')
+        console.log('my stores are', this.stores)
     }
 }
