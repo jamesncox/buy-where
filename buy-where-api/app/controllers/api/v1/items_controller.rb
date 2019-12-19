98b8e36bd222e7097ae948fa6c610062e41523de
jamesncox <jamesncox@yahoo.com>
2019-12-19 09:22:59 -0600

    Remove comments from Item and Stores controllers

--- buy-where-api/app/controllers/api/v1/items_controller.rb
+++ buy-where-api/app/controllers/api/v1/items_controller.rb
@@ -1,27 +1,23 @@
 class Api::V1::ItemsController < ApplicationController
     def index
         @items = Item.all 
-        
-        render json: @items, status: 200
+        render json: @items, include: :store, status: 200
     end
 
     def show
         @item = Item.find_by(id: params[:id])
-       
-        render json: @item, status: 200
+        render json: @item, include: :store, status: 200
     end
 
     def create
         @item = Item.create(items_params)
-
-        render json: @item, status: 200
+        render json: @item, include: :store, status: 200
     end 
 
     def update
         @item = Item.find_by(id: params[:id])
         @item.update(item_params)
-      
-        render json: @item, status: 200
+        render json: @item, include: :store, status: 200
     end
     
     def destroy
--- buy-where-api/app/controllers/api/v1/stores_controller.rb
+++ buy-where-api/app/controllers/api/v1/stores_controller.rb
@@ -2,36 +2,22 @@ class Api::V1::StoresController < ApplicationController
 
     def index
         @stores = Store.all 
-        # options = {
-        #     include: [:items]
-        # }
-        # render json: StoreSerializer.new(@stores, options), status: 200
-        render json: @stores, include: :items,
-         status: 200
+        render json: @stores, include: :items, status: 200
     end
 
     def show
         @store = Store.find_by(id: params[:id])
-        # options = {
-        #     include: [:items]
-        # }
-        # render json: StoreSerializer.new(@store, options), status: 200
         render json: @store, include: :items, status: 200
     end
 
     def create
         @store = Store.create(store_params)
-        # options = {
-        #     include: [:items]
-        # }
-        # render json: StoreSerializer.new(@store, options), status: 200
         render json: @store, include: :items, status: 200
     end 
 
     def update 
         @store = Store.find_by(id: params[:id])
         @store.update(store_params)
-
         render json: @store, include: :items, status: 200
     end
 
@@ -39,7 +25,6 @@ class Api::V1::StoresController < ApplicationController
         @store.destroy
         head :no_content
     end
-
 
     private
 
