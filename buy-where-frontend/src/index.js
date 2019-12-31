ff2d5419420c91c43550b90275b6a2dd042dd5af
jamesncox <jamesncox@yahoo.com>
2019-12-31 13:48:00 -0600

    Remove un-used code from index.js

--- buy-where-frontend/src/index.js
+++ buy-where-frontend/src/index.js
@@ -2,8 +2,3 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // console.log('DOM fully loaded and parsed');
     const app = new App()
 });
-
-// window.onload = function () {
-//     console.log('page loaded')
-//     const app = new App()
-// }
