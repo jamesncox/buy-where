53b5899bafd9150faeefb78c7bd04ed60074d139
jamesncox <jamesncox@yahoo.com>
2020-01-04 09:52:33 -0600

    Remove un-used code

--- buy-where-frontend/src/components/items.js
+++ buy-where-frontend/src/components/items.js
@@ -2,7 +2,6 @@ class Items {
 
     //create and initiliaze objects and methods related to Items class.
     constructor() {
-        this.items = []
         this.adapter = new ItemsAdapter()
     }
 
